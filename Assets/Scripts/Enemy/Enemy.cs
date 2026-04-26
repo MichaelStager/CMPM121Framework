77101370c8464db30160f9ee9fb0af1e87d83828
Michael Stager <61891711+MichaelStager@users.noreply.github.com>
2026-04-25 17:32:29 -0700

    added RPNEvaluator.dll
    also started the enemy.cs script

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,10 +1,20 @@
+using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class Enemy : MonoBehaviour
 {
+    
     string name;
     int sprite;
     int hp;
     int speed;
     int damage;
+
+
+    Dictionary<string, int> test;
+    private void Start()
+    {
+      
+    }
 }
